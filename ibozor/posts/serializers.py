d7a76357242a61a3046ca3b90d753d31c5cb67bd
ilgor <ilgorbek@gmail.com>
2017-06-12 11:20:13 -0500

    clean up

--- ibozor/posts/serializers.py
+++ ibozor/posts/serializers.py
@@ -1,18 +1,12 @@
 from rest_framework import serializers
-from posts.models import Post, LANGUAGE_CHOICES, STYLE_CHOICES
+from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # code = serializers.CharField(style={'base_template': 'textarea.html'})
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'desc', 'price', 'images')
 
     def create(self, validated_data):
         """
@@ -25,9 +19,7 @@ class PostSerializer(serializers.ModelSerializer):
         Update and return an existing `Posts` instance, given the validated data.
         """
         instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
+        instance.code = validated_data.get('desc', instance.desc)
+        instance.code = validated_data.get('price', instance.price)
         instance.save()
         return instance
